f4b7b4aa8fb8e70c64e56affaa83861bfa51711b
Bagus Hary <bagushary79@gmail.com>
2026-06-10 14:22:36 +0700

    feat: add back to top button

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -27,6 +27,19 @@
     @yield('content')
 
     @include('landing.partials.footer')
+
+    {{-- Back to Top Button --}}
+    <button x-data="{ show: false }" x-init="window.addEventListener('scroll', () => { show = window.scrollY > 400 })" x-show="show"
+        x-transition:enter="transition ease-out duration-200" x-transition:enter-start="opacity-0 translate-y-2"
+        x-transition:enter-end="opacity-100 translate-y-0" x-transition:leave="transition ease-in duration-150"
+        x-transition:leave-start="opacity-100 translate-y-0" x-transition:leave-end="opacity-0 translate-y-2"
+        x-on:click="window.scrollTo({ top: 0, behavior: 'smooth' })"
+        class="fixed bottom-6 right-6 z-50 w-10 h-10 bg-blue-600 hover:bg-blue-700 text-white rounded-full shadow-lg flex items-center justify-center transition-colors"
+        style="display: none;" title="Kembali ke atas">
+        <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="2.5" viewBox="0 0 24 24">
+            <polyline points="18 15 12 9 6 15" />
+        </svg>
+    </button>
 </body>
 
 </html>
